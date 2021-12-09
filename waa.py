fa80b2751e5395012a1b0af2a4cef1f8573d3716
Aashutosh Karale <80333681+Aashutosh-922@users.noreply.github.com>
2021-12-09 23:15:54 +0530

    Update waa.py

--- waa.py
+++ waa.py
@@ -1,3 +1,3 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("+91whatsappnumber", "Good Night", 23, 9)
+pywhatkit.sendwhatmsg("+91whatsappnumber", "Good Night", 23, 9)  #Time should be in 24 hour format.
